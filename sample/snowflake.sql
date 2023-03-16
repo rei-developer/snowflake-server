7303cdd734114355187b6c2ba8612e1a39f64216
nakochan <eunnamu094@gmail.com>
2023-03-16 15:48:23 +0900

    remastered

--- sample/snowflake.sql
+++ sample/snowflake.sql
@@ -7,46 +7,45 @@ create table generated_images
     hash    mediumtext                           not null,
     created datetime default current_timestamp() not null,
     updated datetime default current_timestamp() not null,
-    deleted datetime                             null
+    deleted datetime null
 );
 
 create table lovers
 (
     id       bigint auto_increment
         primary key,
-    userId   bigint                                 not null,
-    name     tinytext                               not null,
-    nickname tinytext                               null,
-    sex      tinyint(1) default 1                   not null,
-    age      tinyint    default 18                  not null,
-    hair     tinytext                               not null,
-    face     tinytext                               not null,
-    eyes     tinytext                               not null,
-    nose     tinytext                               not null,
-    mouth    tinytext                               not null,
-    ears     tinytext                               not null,
-    body     tinytext                               not null,
-    breast   tinytext                               not null,
-    `rank`   tinytext   default 'normal'            null,
-    level    tinyint    default 1                   not null,
-    exp      bigint     default 0                   not null,
-    remarks  mediumtext                             null,
-    isNft    tinyint(1) default 0                   not null,
-    created  datetime   default current_timestamp() not null,
-    updated  datetime   default current_timestamp() not null,
-    deleted  datetime                               null,
-    race     tinytext   default 'human'             not null
-)
-    comment '연인';
+    userId   bigint                               not null,
+    name     tinytext                             not null,
+    nickname tinytext null,
+    race     tinytext default 'human'             not null,
+    sex      tinyint(1) default 1 not null,
+    age      tinyint  default 18                  not null,
+    hair     tinytext                             not null,
+    face     tinytext                             not null,
+    eyes     tinytext                             not null,
+    nose     tinytext                             not null,
+    mouth    tinytext                             not null,
+    ears     tinytext                             not null,
+    body     tinytext                             not null,
+    breast   tinytext                             not null,
+    `rank`   tinytext default 'normal' null,
+    level    tinyint  default 1                   not null,
+    exp      bigint   default 0                   not null,
+    remarks  mediumtext null,
+    isNft    tinyint(1) default 0 not null,
+    created  datetime default current_timestamp() not null,
+    updated  datetime default current_timestamp() not null,
+    deleted  datetime null
+) comment '연인';
 
 create table users
 (
     id      bigint auto_increment
         primary key,
-    uid     tinytext                               not null,
-    name    tinytext                               not null,
-    sex     tinyint(1) default 0                   not null,
-    created datetime   default current_timestamp() not null,
-    updated datetime   default current_timestamp() not null on update current_timestamp(),
-    deleted datetime                               null
+    uid     tinytext                             not null,
+    name    tinytext                             not null,
+    sex     tinyint(1) default 0 not null,
+    created datetime default current_timestamp() not null,
+    updated datetime default current_timestamp() not null on update current_timestamp (),
+    deleted datetime null
 );
